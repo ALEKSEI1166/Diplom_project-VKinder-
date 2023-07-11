00cc17e78a42399243aa1673c7119271b8f12dbe
Алексей Долгов <dolgovas40@yandex.ru>
2023-07-11 23:07:59 +0300

    Начальная фиксация.

--- data_store.py
+++ data_store.py
@@ -95,8 +95,8 @@ if __name__ == '__main__':
     # delete_db(conn)
     # print("БД удалена")
     # Base.metadata.delete_all(engine)
-    Base.metadata.drop_all(engine) # удалить таблицы
-    Base.metadata.create_all(engine)  # создать таблицы
+    Base.metadata.drop_all(engine) # удалить таблицу
+    Base.metadata.create_all(engine)  # создать таблицу
     add_user(engine, '1234', '567890')  #добавить user
     res = check_user(engine, '1234', '567890')
     print(res)
