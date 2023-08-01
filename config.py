5ddc411f70a8309959183afedf8a22b3cc43ef50
ALEKSEI1166 <112812931+ALEKSEI1166@users.noreply.github.com>
2023-08-01 19:51:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #ключ доступа к сообществу VK:
-comunity_token = 'vk1.a.0o6R2AN86uVlOwG41bkraDGCMMteu5znJxuJkHfnySjqWqeWszNvk---7GtlNpknN62C8Ge8kT1PhMwSIsHiJzePG0egzyZezgrWFcCA05Mo9z2eyXRWLByTcdSedvK_HvSAFmBi_mgbtNTXAFo85Ro6TJ5y9X2HQHlOBEF0o9oaaahrFZVJrn_HxpbH8vP3Wk-W27RAY87SqBfOsG10HA'
+comunity_token = ' '
 
 #ключ пользователя VK:
-acces_token = 'vk1.a.YYwpjFjoCyEGmXpFJLXmowJa-axKiZ-_-N-HXKcY068VuaQhQfqlckqlaf8MAhGyWfnWHsNxyO2qGFpDbjgZ_kj0d1SR0b5QoRVEwyJqyGlrKu--8c9AIe672jAp8BL9hUKmiX6IJxxVd0-CGXV8YA0neY-_iSmnNlskKpOfyHlaxqcGXBT9In_qGtVFktdRAH7eAeYdAjoallSlhVwXgA'
+acces_token = ' '
 
 #путь к БД:
 db_url_object = "postgresql+psycopg2://postgres:603306MuS@localhost/my_chatbot"
